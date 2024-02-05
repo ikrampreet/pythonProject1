914fdda26e99ff98d19e7f2adf88c813673654eb
Ikrampreet kaur <ikrampreetdhaliwal@gmail.com>
2024-02-05 10:22:33 +0200

    new commit

--- examples.py
+++ examples.py
@@ -1,4 +1,6 @@
-num = 0
-while num < 12:
-    print("Have a Nice Day")
-    num + =1
+while True:
+    user_input = float(input("Enter a number:"))
+
+    if user_input < 0:
+        print("Negative number entered . stoping the program.")
+        break
